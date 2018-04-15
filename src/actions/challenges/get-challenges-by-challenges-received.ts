f938cf0dc5aa3958e96e70da9f74a7be00636535
Deshdeep Mandal <deshdeep.mandal@powerschool.com>
2018-04-15 11:04:10 +0530

    Add challenges actions files

---
+++ src/actions/challenges/get-challenges-by-challenges-received.ts
@@ -0,0 +1,51 @@
+import * as exp from 'express';
+import {
+  ForbiddenError,
+  IAuthenticatedRequest,
+  ILoggerFactory,
+  NotFoundError,
+  BadRequestError,
+  USER_TYPE_TEACHER
+} from 'pbis-common';
+/* import { IInterventionSummary } from '../../models';
+import { IInterventionService } from '../../services/intervention'; */
+import * as validate from '../../services/validation';
+import * as uidUtil from 'library-uid';
+
+export function getChallengesByChallengesReceivedRouteHandler(req: IAuthenticatedRequest, res: exp.Response): void {
+  const iocContainer = req.requestIocContainer;
+  const log = iocContainer.get<ILoggerFactory>(Symbol.for('ILoggerFactory')).getLogger('action.challenges.getChallengesByChallengesReceivedRouteHandler');
+
+  // Parse params
+  let userUid = req.params.uid.trim();
+  if (!validate.isValidUid(userUid)) {
+    res.status(400).send('Invalid intervention uid');
+    return;
+  }
+  //interventionUid = uidUtil.addDashes(interventionUid);
+
+  //TODO: Discuss if we need userType validation
+  /* if (!(req.userPersona.userType === USER_TYPE_TEACHER)) {
+    res.status(403).send('Bad request, userType ' + req.userPersona.userType + ' is not authorised to view this intervention.');
+    return;
+  } */
+
+  //TODO: call service
+  /* let interventionService = iocContainer.get<IInterventionService>(Symbol.for('IInterventionService'));
+  interventionService.getIntervention(districtUid, interventionUid)
+    .then((results: IInterventionSummary) => {
+      return res.type('json').send(results);
+    })
+    .catch((error) => {
+      if (error instanceof NotFoundError) {
+        res.status(404).send(error.message);
+      } else if (error instanceof ForbiddenError) {
+        res.status(403).send(error.message);
+      } else if (error instanceof BadRequestError) {
+        res.status(400).send(error.message);
+      } else {
+        log.error(error);
+        res.status(500).send(error);
+      }
+    }); */
+}
---
+++ src/actions/challenges/init.ts
@@ -0,0 +1,165 @@
+import * as exp from 'express';
+import { Kernel } from 'inversify';
+import {
+  IAuthenticatedRequest,
+  IEnforceAuthenticationHandlerFactory
+} from 'pbis-common';
+import * as middleware from '../../middleware';
+
+import * as postChallengeRoute from './post-challenge';
+import * as getChallengeRoute from './get-challenges';
+import * as putChallengeRoute from './put-challenge';
+import * as getChallengeByRequestedRoute from './get-challenges-by-challenges-requested';
+import * as getChallengeByReceivedRoute from './get-challenges-by-challenges-received';
+
+export function init(app: exp.Application, kernel: Kernel): void {
+  const mustBeAuthenticated = kernel.get<IEnforceAuthenticationHandlerFactory>(Symbol.for('IEnforceAuthenticationHandlerFactory'));
+
+  //TODO: Fix this
+  /**
+   * @swagger
+   * definitions:
+   *   User:
+   *     type: object
+   *     required:
+   *       - uid
+   *     properties:
+   *       userName:
+   *         type: string
+   *   Users:
+   *      type: array
+   *      items: 
+   *        $ref: '#/definitions/User'
+
+  /**
+   * @swagger
+   * /quizUp/v1/user:
+   *   post:
+   *     description: Creates a new user
+   *     consumes:
+   *       - application/json
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: input
+   *         in: body
+   *         description: Input data to create a user
+   *         required: true
+   *         schema:
+   *           $ref: '#/definitions/User'
+   *     responses:
+   *       201:
+   *         description: User was created
+   *         schema:
+   *           $ref: '#/definitions/User'
+   *       400:
+   *         description: Invalid user uid
+   *     tags:
+   *       - User
+   */
+  app.post(
+    '/quizUp/v1/leaderboard',
+    middleware.authenticatedMiddlewares(mustBeAuthenticated),
+    (req: IAuthenticatedRequest, res: exp.Response) => {
+      postLeaderboardRoute.postLeaderboardRouteHandler(req, res);
+    });
+
+  /**
+   * @swagger
+   * /quizUp/v1/user:
+   *   get:
+   *     description: Gets all users
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: List of users
+   *         schema:
+   *           $ref: '#/definitions/InterventionSummary'
+   *     tags:
+   *       - User
+   */
+  app.get(
+    `/quizUp/v1/leaderboard`,
+    middleware.authenticatedMiddlewares(mustBeAuthenticated),
+    (req: IAuthenticatedRequest, res: exp.Response) => {
+      getLeaderboardRoute.getLiveUserRouteHandler(req, res);
+    });
+
+  /**
+   * @swagger
+   * /quizUp/v1/user:
+   *   get:
+   *     description: Gets all users
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: List of users
+   *         schema:
+   *           $ref: '#/definitions/InterventionSummary'
+   *     tags:
+   *       - User
+   */
+  app.get(
+    `/quizUp/v1/leaderboard`,
+    middleware.authenticatedMiddlewares(mustBeAuthenticated),
+    (req: IAuthenticatedRequest, res: exp.Response) => {
+      getLeaderboardRoute.getLiveUserRouteHandler(req, res);
+    });
+
+  /**
+   * @swagger
+   * /quizUp/v1/user:
+   *   get:
+   *     description: Gets all users
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: List of users
+   *         schema:
+   *           $ref: '#/definitions/InterventionSummary'
+   *     tags:
+   *       - User
+   */
+  app.get(
+    `/quizUp/v1/leaderboard`,
+    middleware.authenticatedMiddlewares(mustBeAuthenticated),
+    (req: IAuthenticatedRequest, res: exp.Response) => {
+      getLeaderboardRoute.getLiveUserRouteHandler(req, res);
+    });
+  
+  //TODO: Fix this swagger comment
+  /**
+   * @swagger
+   * /quizUp/v1/user/student/{studentUid}:
+   *   get:
+   *     description: Gets a list of interventions for a student
+   *     security:
+   *       - Bearer: []
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: studentUid
+   *         in: path
+   *         description: The studentUid of the student
+   *         required: true
+   *         type: string
+   *     responses:
+   *       200:
+   *         description: A list of interventions for the student
+   *         schema:
+   *            $ref: '#/definitions/InterventionSummaries'
+   *       400:
+   *         description: Invalid student uid
+   *     tags:
+   *       - Intervention
+   */
+  app.put(
+    '/quizUp/v1/leaderboard/:leaderboardUid',
+    middleware.authenticatedMiddlewares(mustBeAuthenticated),
+    (req: IAuthenticatedRequest, res: exp.Response) => {
+      putLeaderboardRoute.putLeaderboardRouteHandler(req, res);
+    });
+}
